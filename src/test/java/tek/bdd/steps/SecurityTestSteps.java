327f7cea80c9b594ec442175efc626c88f4acea6
alimu <alimuhamad.safa@gmail.com>
2024-10-11 15:21:16 -0400

    Finished the First test case

--- src/test/java/tek/bdd/steps/SecurityTestSteps.java
+++ src/test/java/tek/bdd/steps/SecurityTestSteps.java
@@ -3,7 +3,6 @@ package tek.bdd.steps;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import tek.bdd.pages.HomePage;
 import tek.bdd.pages.SignInPage;
 import tek.bdd.utility.SeleniumUtility;
@@ -20,16 +19,29 @@ public class SecurityTestSteps extends SeleniumUtility {
         String pageSubTitle = getElementText(SignInPage.PAGE_SUBTITLE);
         Assert.assertEquals("Sign in", pageSubTitle);
     }
+    @When("user enter username and password and click on login")
+    public void userEnterAndClickOnLogin() {
+        sendText(SignInPage.EMAIL_INPUT,"mohammad2536@gmail.com");
+        sendText(SignInPage.PASSWORD_INPUT,"Password@123");
+        clickOnElement(SignInPage.LOGIN_BUTTON);
+    }
+
     @When("user enter {string} and {string} and click on login")
-    public void userEnterAndClickOnLogin(String email, String password) {
+    public void userEnterAndClickOnLoginWithInvalid(String email, String password) {
         sendText(SignInPage.EMAIL_INPUT,email);
         sendText(SignInPage.PASSWORD_INPUT,password);
         clickOnElement(SignInPage.LOGIN_BUTTON);
     }
+
+    @Then("user should see error {string}")
+    public void user_should_see_error(String expectedErrorMessage) {
+        String actualErrorMessage = getElementText(SignInPage.ERROR_MESSAGE);
+        Assert.assertEquals(expectedErrorMessage,actualErrorMessage);
+    }
+
     @Then("user should be able to see account link")
     public void userShouldBeAbleToSeeAccountLink() {
         boolean isAccountDisplayed = isElementDisplayed(HomePage.ACCOUNT_LINK);
         Assert.assertTrue(isAccountDisplayed);
-
     }
 }
--- src/test/resources/features/SecurityTest.feature
+++ src/test/resources/features/SecurityTest.feature
@@ -5,3 +5,15 @@ Feature: validating the Sign In to Retail app
     Then validate user is in sign in page
     When user enter "mohammad2536@gmail.com" and "Password@123" and click on login
     Then user should be able to see account link
+
+  Scenario Outline:  Navigate to sign in page and sign in with invalid credentials
+    When user click on sign in link
+    Then validate user is in sign in page
+    When user enter "<username>" and "<password>" and click on login
+    Then user should see error "<errorMessage>"
+    Examples:
+    |       username         |   password     |      errorMessage         |
+    |mohammad2536@gmail.com| WrongPassword  |wrong username or password|
+    |Wrong@gmail.com        | Password@123  |wrong username or password|
+    |mohammad2536@gmail.com| Passwosrd@123s |wrong username or password|
+    |invalid@gmail.com        |Password@123  |wrong username or password|
